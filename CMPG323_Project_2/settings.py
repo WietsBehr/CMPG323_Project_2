c8ce85acbd0cf1497750c6d3026e73d9b17fda3d
WietsBehr (31770509) <70149733+WietsBehr@users.noreply.github.com>
2021-11-19 18:28:28 +0200

    setting update rm localhost

--- CMPG323_Project_2/settings.py
+++ CMPG323_Project_2/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-l7bw69=mk7a*h)pwdo&k+)89)+vigpblhflk6s5q4ethlv_1r6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cmpg323photogallery.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['cmpg323photogallery.herokuapp.com']
 
 
 # Application definition

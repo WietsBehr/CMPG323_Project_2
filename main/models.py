a3793eccb3e97c39a18f563ba3178704ef7b5619
WietsBehr (31770509) <70149733+WietsBehr@users.noreply.github.com>
2021-11-19 08:58:59 +0200

    Added AWS settings

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
 
 class Photo(models.Model):
     title = models.CharField(max_length=100)
@@ -8,4 +9,10 @@ class Photo(models.Model):
     captured_date = models.DateTimeField(default=timezone.now)
     image = models.ImageField(upload_to="media/")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return  f'{self.user.username} Photo'
+
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 

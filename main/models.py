df7e3fedd2f728d8b8484c25e4b28f654ad9af3a
WietsBehr (31770509) <70149733+WietsBehr@users.noreply.github.com>
2021-11-21 07:46:02 +0200

    default value for time

--- main/models.py
+++ main/models.py
@@ -7,7 +7,7 @@ from PIL import Image
 class Photo(models.Model):
     title = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    captured_date = models.DateTimeField()
+    captured_date = models.DateTimeField(default=timezone.now())
     image = models.ImageField(upload_to="media/")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
